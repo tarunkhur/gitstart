a4d3e022097fb81605d2a4c8a0b89181e8fa7f98
tarunkhur <tarunk.0810@gmail.com>
2023-01-13 22:26:23 +0530

    new change

---
+++ dom.js
@@ -0,0 +1,7 @@
+let headerTitle=document.getElementById('header-title');
+let header=document.getElementById('main-header');
+let a=document.querySelector('.title');
+headerTitle.textContent="Hii";
+header.style.border='solid 3px black'
+a.style.fontWeight='bold';
+a.style.color='green'
