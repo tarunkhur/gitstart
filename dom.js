1326a652c2bcad5cc79eb4cbcee9862dab78b451
tarunkhur <tarunk.0810@gmail.com>
2023-01-13 22:37:05 +0530

    updated

--- dom.js
+++ dom.js
@@ -5,3 +5,9 @@ headerTitle.textContent="Hii";
 header.style.border='solid 3px black'
 a.style.fontWeight='bold';
 a.style.color='green'
+
+let item=document.getElementsByClassName('list-group-item')
+item[2].style.backgroundColor='green'
+for(let i=0;i<item.length;i++){
+    item[i].style.fontWeight='bold';
+}
