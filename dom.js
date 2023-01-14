896a38c06bfede456864326ab71c41283fb80e26
tarunkhur <tarunk.0810@gmail.com>
2023-01-14 12:04:20 +0530

    latest

--- dom.js
+++ dom.js
@@ -7,14 +7,26 @@ header.style.border='solid 3px black'
 a.style.fontWeight='bold';
 a.style.color='green'
 
-let item=document.getElementsByClassName('list-group-item')
-item[2].style.backgroundColor='green'
-for(let i=0;i<item.length;i++){
-    item[i].style.fontWeight='bold';
-}
+// let item=document.getElementsByClassName('list-group-item')
+// item[2].style.backgroundColor='green'
+// for(let i=0;i<item.length;i++){
+//     item[i].style.fontWeight='bold';
+// }
 
-let li=document.getElementsByTagName('li')
-li[2].style.backgroundColor='yellow'
-for(let i=0;i<li.length;i++){
-    li[i].style.fontWeight='bold';
+// let li=document.getElementsByTagName('li')
+// li[2].style.backgroundColor='yellow'
+// for(let i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+
+// let second=document.querySelector('li:nth-child(2)')
+// second.style.backgroundColor='green'
+
+let odd=document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green'
 }
+document.querySelectorAll('li')[1].style.backgroundColor='green'
+
+let third=document.querySelector('li:nth-child(3)');
+third.style.display="none";
